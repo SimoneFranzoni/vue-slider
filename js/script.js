f6d3f4ff2649155d45eab8d90d7f1b97c1e7a1a5
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-11 16:23:35 +0100

    first-commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+
+    el: '#app',
+    data:{
+        images: ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'],
+        counter: 0
+    },
+
+    methods:{
+        nextSlide(){
+            if(this.counter > this.images.lenght - 1){
+                this.counter = 0;
+            } else {
+                this.counter++;
+            }
+        },
+    
+        prevSlide(){
+            if(this.counter < 0){
+                this.counter = this.image.lenght - 1;
+            } else {
+                this.counter--;
+            }
+            console.log(counter);
+        }
+    }
+})
